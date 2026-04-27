5257d4addcafcf3bf8c574a94357b44929c04b1a
ruudvan03 <ordazruudvan@gmail.com>
2026-04-27 16:06:08 -0600

    Funcionando

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - La Casona</title>
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-screen w-full overflow-hidden font-sans">
 
